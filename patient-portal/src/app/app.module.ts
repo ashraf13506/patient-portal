d003a284df8bb6890fa8d8a9f1153f7110dd41c8
ashraf13506 <ashraf13506@gmail.com>
2023-12-28 23:10:01 +0530

    added_registration_login_form_styling

--- patient-portal/src/app/app.module.ts
+++ patient-portal/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './structure/header/header.component';
+import { FooterComponent } from './structure/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
