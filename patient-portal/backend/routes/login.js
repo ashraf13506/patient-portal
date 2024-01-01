2edccd6987456fbeb14d5a801f026e72d07ca677
ashraf13506 <ashraf13506@gmail.com>
2024-01-01 22:57:49 +0530

    added_header_footer_templated

--- patient-portal/backend/routes/login.js
+++ patient-portal/backend/routes/login.js
@@ -1,17 +1,32 @@
 const express=require('express');
 const router=express.Router();
-const loginModel=require('../../backend/schemas/login-schema')
+const loginModel=require('../../backend/schemas/login-schema');
+const patientSchema=require('./../schemas/registration-schema');
+const bcrypt=require('bcrypt')
+
 
 
 router.post('/',async(req,res)=>{
+  const {username,password}=req.body;
 
-  const result=await loginModel.create({
-    username:req.body.username,
-    password:req.body.password
-
+  const user= patientSchema.find({
+    email:username
   })
+  const hashedpassword=await bcrypt.compare(password,user.setpassword,(err,res)=>{
+    if(res){
+      return res.json({message:"success"})
+    }
+  })
+  console.log(hashedpassword)
 
-  res.status(201).json({message:"success"})
+  // const result=await patientSchema.create({
+  //   username:req.body.username,
+  //   password:req.body.password,
+  //   setpassword:"test"
+
+  // })
+
+
 
 
 })
